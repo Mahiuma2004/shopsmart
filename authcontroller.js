4c720afb36d384f5e76f7e5699031ba1f0eb967d
Mahiuma2004 <162769674+Mahiuma2004@users.noreply.github.com>
2025-06-27 10:35:36 +0530

    Create authcontroller.js

---
+++ authcontroller.js
@@ -0,0 +1,22 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+
+exports.register = async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) return res.status(400).json({ msg: 'User already exists' });
+
+  const user = await User.create({ name, email, password });
+  res.json({ user, token: generateToken(user._id) });
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user || !(await user.matchPassword(password)))
+    return res.status(401).json({ msg: 'Invalid credentials' });
+
+  res.json({ user, token: generateToken(user._id) });
+};
